b6980b3607cace2b123273be47c489c8300f97b8
Jean Bitencourt <jeanbitencourt55@gmail.com>
2021-04-26 11:59:51 -0300

    Criação do ChallengeContoller e reatividade dos componentes

--- lib/challenge/widgets/question_indicator/question_indicator_widget.dart
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -3,6 +3,15 @@ import 'package:devquiz/shared/widgets/progress_indicator_widget/progress_indica
 import 'package:flutter/material.dart';
 
 class QuestionIndicatorWidget extends StatelessWidget {
+  final int currentPage;
+  final int length;
+
+  const QuestionIndicatorWidget({
+    Key? key,
+    required this.currentPage,
+    required this.length,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -14,11 +23,11 @@ class QuestionIndicatorWidget extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  "Questão 04",
+                  "Questão $currentPage",
                   style: AppTextStyles.body,
                 ),
                 Text(
-                  "de 10",
+                  "de $length",
                   style: AppTextStyles.body,
                 ),
               ],
@@ -27,7 +36,7 @@ class QuestionIndicatorWidget extends StatelessWidget {
               height: 16,
             ),
             ProgressIndicatorWidget(
-              value: 0.7,
+              value: currentPage / length,
             ),
           ],
         ),
--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,25 +1,57 @@
-import 'package:devquiz/challenge/widgets/answer/answer_widget.dart';
-import 'package:devquiz/core/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
-  final String title;
+import 'package:devquiz/challenge/widgets/answer/answer_widget.dart';
+import 'package:devquiz/core/app_text_styles.dart';
+import 'package:devquiz/shared/models/answer_model.dart';
+import 'package:devquiz/shared/models/question_model.dart';
 
-  const QuizWidget({required this.title}) : super();
+class QuizWidget extends StatefulWidget {
+  final QuestionModel question;
+  final VoidCallback onChange;
+
+  const QuizWidget({
+    Key? key,
+    required this.question,
+    required this.onChange,
+  }) : super();
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AnswerModel answer(int index) => widget.question.answers[index];
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
+          SizedBox(
+            height: 64,
+          ),
           Text(
-            title,
+            widget.question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(
             height: 24,
           ),
-          AnswerWidget(title: "title", isRight: true, isSelected: true),
+          for (var i = 0; i < widget.question.answers.length; i++)
+            AnswerWidget(
+              anser: answer(i),
+              disabled: indexSelected != -1,
+              isSelected: indexSelected == i,
+              onTap: () {
+                setState(() {
+                  indexSelected = i;
+                });
+                Future.delayed(Duration(seconds: 1))
+                    .then((value) => widget.onChange);
+              },
+            ),
         ],
       ),
     );
